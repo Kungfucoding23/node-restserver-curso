f7d521562d145c51b2bc1550720ea0f254069c6b
Alejandro <alesantin012@hotmail.com>
2021-01-24 21:08:51 -0300

    uploads

---
+++ server/routes/upload.js
@@ -0,0 +1,114 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+const Usuario = require('../models/usuario')
+const fs = require('fs')
+const path = require('path')
+
+// default options
+app.use(fileUpload({ useTempFiles: true }))
+
+app.put('/upload/:tipo/:id', (req, res) => {
+
+    let tipo = req.params.tipo
+    let id = req.params.id
+
+    if (!req.files) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'No se a cargado ningun archivo'
+            }
+        })
+    }
+
+    //Valida tipo
+    let tiposValidos = ['productos', 'usuarios']
+    if (tiposValidos.indexOf(tipo) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son ' + tiposValidos.join(', ')
+            }
+        })
+    }
+
+    let archivo = req.files.archivo
+    let nombreCortado = archivo.name.split('.')
+    let extension = nombreCortado[nombreCortado.length - 1]
+
+    // Extensiones permitidas
+    let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg']
+
+    if (extensionesValidas.indexOf(extension) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Las extensiones permitidas son ' + extensionesValidas.join(', '),
+                ext: extension
+            }
+        })
+    }
+
+    // cambiar nombre al archivo
+    let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`
+
+    archivo.mv(`uploads/${tipo}/${nombreArchivo}`, err => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+        //Si llega hasta aca quiere decir que se subio el archivo exitosamente
+        imagenUsuario(id, res, nombreArchivo)
+    })
+})
+
+function imagenUsuario(id, res, nombreArchivo) {
+    Usuario.findById(id, (err, usuarioDB) => {
+        if (err) {
+            borraArchivo(nombreArchivo, 'usuarios')
+            return res.status(500).json({
+                ok: false,
+                err
+            })
+        }
+
+        if (!usuarioDB) {
+            borraArchivo(nombreArchivo, 'usuarios')
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no existe'
+                }
+            })
+        }
+
+        // verifica la ruta del archivo
+        borraArchivo(usuarioDB.img, 'usuarios')
+
+        usuarioDB.img = nombreArchivo
+        usuarioDB.save((err, usuarioGuardado) => {
+            res.json({
+                ok: true,
+                usuario: usuarioGuardado,
+                img: nombreArchivo
+            })
+        })
+    })
+}
+
+function imagenProducto() {
+
+}
+
+function borraArchivo(nombreImagen, tipo) {
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${nombreImagen}`)
+    if (fs.existsSync(pathImagen)) {
+        //EN CASO DE QUE EXISTA BORRAR ESTE PATH
+        fs.unlinkSync(pathImagen)
+    }
+}
+
+module.exports = app
